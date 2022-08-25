d325ab642aaf417163ee9b3d57e6f99054770974
julioconceicao <julioconceicao.gtr@gmail.com>
2022-08-25 09:33:22 -0300

    fix: fixed BookService + re-implemented automapper

--- LibraryProjectSolution/LibraryProject.Domain/Interfaces/Implementations/BookServices.cs
+++ LibraryProjectSolution/LibraryProject.Domain/Interfaces/Implementations/BookServices.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoMapper;
 using LibraryProject.Domain.Responses;
 using LibraryProject.Infrastructure;
 
@@ -10,13 +11,16 @@ namespace LibraryProject.Domain.Interfaces.Implementations
     public class BookServices : IBookService
     {
         private readonly IBookRepository _bookRepository;
-        public BookServices(IBookRepository bookRepository)
+        private readonly IMapper _mapper;
+
+        public BookServices(IBookRepository bookRepository, IMapper mapper)
         {
+            _mapper = mapper;
             _bookRepository = bookRepository;
         }
         public async Task<CreateBookResponse> CreateBookAsync(BookModel book)
         {
-            var response = new CreateBookResponse();
+            var response = _mapper.Map<CreateBookResponse>(book);
             await _bookRepository.CreateBookAsync(book);
             return response;
         }
