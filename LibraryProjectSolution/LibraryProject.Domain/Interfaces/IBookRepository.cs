fd27f049783b4a0c232fc34c7b011d7308386cc1
julioconceicao <julioconceicao.gtr@gmail.com>
2022-09-15 22:47:35 -0300

    delete: to merge stable development branch

--- LibraryProjectSolution/LibraryProject.Domain/Interfaces/IBookRepository.cs
+++
@@ -1,9 +0,0 @@
-using LibraryProject.Domain.Interfaces.GenericInterfaces;
-using LibraryProject.Infrastructure;
-
-namespace LibraryProject.Domain.Interfaces
-{
-    public interface IBookRepository :IRepository<BookModel>
-    {
-    }
-}
